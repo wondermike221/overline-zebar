dca2c0214e200f9bc7a51c1ab8241fe5a6be45c3
Ashutosh Sharma <asrma.sharma@gmail.com>
2025-05-26 08:36:49 -0400

    Fix width issue for wide screens

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
   const statIconClassnames = "h-3 w-3 text-icon";
 
   return (
-    <div className="relative flex justify-between items-center bg-background/80 border border-button-border/80 backdrop-blur-3xl text-text h-full antialiased select-none rounded-lg font-mono py-1.5">
+    <div className="relative flex justify-between items-center bg-background/80 border border-button-border/80 backdrop-blur-3xl text-text h-full antialiased select-none rounded-lg font-mono py-1.5 mx-1">
       <div className="flex items-center gap-2 h-full z-10 pl-1.5">
         <div className="flex items-center gap-1.5 h-full">
           <TilingControl glazewm={output.glazewm} />
@@ -101,7 +101,6 @@ function App() {
             statIconClassnames={statIconClassnames}
           />
         </div>
-
 
         <div className="h-full flex items-center px-0.5 pr-1">
           <Systray systray={output.systray} />
